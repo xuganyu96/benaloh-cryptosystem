5d95c6e5cc19fcae3e2c004497ec1610f7e91059
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2023-11-15 10:46:56 -0500

    Proof of being r-th residue

--- src/proofs/mod.rs
+++ src/proofs/mod.rs
@@ -3,3 +3,4 @@
 pub mod ballot;
 pub mod decide; // prove that residue class can be correctly decided
 pub mod rc; // prove that the residue class known // prove that residue class is one of among a specified set of possibilities
+pub mod tally;
